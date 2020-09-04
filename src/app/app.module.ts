aeffdcc4077374d08ee913955953d7e25ac8513c
Jan-Thees <jt.plennis@gmail.com>
2020-09-04 10:11:00 +0200

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ArticleDetailComponent
   ],
   imports: [
     BrowserModule
